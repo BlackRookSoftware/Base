9dbc3deef0701685bc770690310661b095209d63
mtrop <mtropiano@gmail.com>
2020-03-01 14:43:35 -0500

    Use origin set class for new sets

--- src/main/java/com/blackrook/base/util/SetUtils.java
+++ src/main/java/com/blackrook/base/util/SetUtils.java
@@ -5,7 +5,7 @@
  ******************************************************************************/
 package com.blackrook.base.util;
 
-import java.util.HashSet;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
 
 /**
@@ -14,6 +14,38 @@ import java.util.Set;
  */
 public final class SetUtils
 {
+	/**
+	 * Creates a new instance of a class from a class type.
+	 * This essentially calls {@link Class#getDeclaredConstructor(Class...)} with no arguments 
+	 * and {@link Class#newInstance()}, but wraps the call in a try/catch block that only throws an exception if something goes wrong.
+	 * @param <T> the return object type.
+	 * @param clazz the class type to instantiate.
+	 * @return a new instance of an object.
+	 * @throws RuntimeException if instantiation cannot happen, either due to
+	 * a non-existent constructor or a non-visible constructor.
+	 */
+	private static <T> T create(Class<T> clazz)
+	{
+		Object out = null;
+		try {
+			out = clazz.getDeclaredConstructor().newInstance();
+		} catch (SecurityException ex) {
+			throw new RuntimeException(ex);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		} catch (InstantiationException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalArgumentException e) {
+			throw new RuntimeException(e);
+		} catch (InvocationTargetException e) {
+			throw new RuntimeException(e);
+		} catch (NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return clazz.cast(out);
+	}
+	
 	/**
 	 * Returns a new Set that is the union of the objects in two sets,
 	 * i.e. a set with all objects from both sets.
@@ -26,7 +58,7 @@ public final class SetUtils
 	@SuppressWarnings("unchecked")
 	public static <T, S extends Set<T>> S union(S set1, S set2)
 	{
-		Set<T> out = new HashSet<T>();
+		Set<T> out = create(set1.getClass());
 		for (T val : set1)
 			out.add(val);
 		for (T val : set2)
@@ -46,7 +78,7 @@ public final class SetUtils
 	@SuppressWarnings("unchecked")
 	public static <T, S extends Set<T>> S intersection(S set1, S set2)
 	{
-		Set<T> out = new HashSet<T>();
+		Set<T> out = create(set1.getClass());
 		
 		S bigset = set1.size() > set2.size() ? set1 : set2;
 		S smallset = bigset == set1 ? set2 : set1;
@@ -71,7 +103,7 @@ public final class SetUtils
 	@SuppressWarnings("unchecked")
 	public static <T, S extends Set<T>> S difference(S set1, S set2)
 	{
-		Set<T> out = new HashSet<T>();
+		Set<T> out = create(set1.getClass());
 		for (T val : set1)
 		{
 			if (!set2.contains(val))
@@ -91,7 +123,7 @@ public final class SetUtils
 	@SuppressWarnings("unchecked")
 	public static <T, S extends Set<T>> S xor(S set1, S set2)
 	{
-		Set<T> out = new HashSet<T>();
+		Set<T> out = create(set1.getClass());
 		for (T val : set1)
 		{
 			if (!set2.contains(val))

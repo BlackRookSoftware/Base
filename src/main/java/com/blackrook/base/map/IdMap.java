494d0b2e64a80939c8da5d2e73443eb3392618b7
mtrop <mtropiano@gmail.com>
2022-01-03 23:19:29 -0500

    Loader fix and IdMap

---
+++ src/main/java/com/blackrook/base/map/IdMap.java
@@ -0,0 +1,66 @@
+package com.blackrook.base.map;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A map that is used to produce unique instances of
+ * very mutable objects. 
+ * <p> The direct need for this is being able to use mutual exclusion 
+ * (<code>synchronized</code>) on objects that would have strict equality
+ * but tend to have a lack of uniqueness in reference, like Strings or Integers
+ * or other common classes or boxed primitives.
+ * @author Matthew Tropiano
+ * @param <T> the type that this map holds.
+ */
+public class IdMap<T> 
+{
+	/** Reference map. */
+	private Map<T, WeakReference<T>> refMap;
+	
+	/**
+	 * Creates a new IdMap.
+	 */
+	public IdMap()
+	{
+		this.refMap = new HashMap<>();
+	}
+	
+	/**
+	 * Gets the unique reference for a specific value instance.
+	 * This method is thread-safe.
+	 * @param value the value to use.
+	 * @return a common reference for the provided value.
+	 * @see #equals(Object)
+	 * @see #hashCode()
+	 */
+	public T uniqueRef(T value)
+	{
+		WeakReference<T> out;
+		synchronized (refMap)
+		{
+			if ((out = refMap.get(value)) == null)
+				refMap.put(value, out = new WeakReference<T>(value));
+		}
+		return out.get();
+	}
+	
+	/**
+	 * Removes the reference for a value.
+	 * If the entry does not exist, this does nothing.
+	 * This method is thread-safe.
+	 * @param value the value to use.
+	 */
+	public void releaseRef(T value)
+	{
+		if (!refMap.containsKey(value))
+			return;
+		
+		synchronized (refMap) 
+		{
+			refMap.remove(value);
+		}
+	}
+	
+}

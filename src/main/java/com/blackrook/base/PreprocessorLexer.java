1c6085a60f460d5016f1ebf89dd56812cc1eb41a
mtrop <mtropiano@gmail.com>
2022-01-04 20:50:40 -0500

    Update copyright header

--- src/main/java/com/blackrook/base/PreprocessorLexer.java
+++ src/main/java/com/blackrook/base/PreprocessorLexer.java
@@ -1,9 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2017-2020 Black Rook Software
- * This program and the accompanying materials are made available under the 
- * terms of the GNU Lesser Public License v2.1 which accompanies this 
- * distribution, and is available at 
- * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * Copyright (c) 2019-2021 Black Rook Software
+ * This program and the accompanying materials are made available under 
+ * the terms of the MIT License, which accompanies this distribution.
  ******************************************************************************/
 package com.blackrook.base;
 
--- src/test/java/com/blackrook/base/util/WhereTest.java
+++ src/test/java/com/blackrook/base/util/WhereTest.java
@@ -1,3 +1,8 @@
+/*******************************************************************************
+ * Copyright (c) 2019-2021 Black Rook Software
+ * This program and the accompanying materials are made available under 
+ * the terms of the MIT License, which accompanies this distribution.
+ ******************************************************************************/
 package com.blackrook.base.util;
 
 public final class WhereTest 

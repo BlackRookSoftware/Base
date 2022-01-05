90052b93ca70594aec6992d5620dab364cca1fd9
mtrop <mtropiano@gmail.com>
2022-01-04 22:57:55 -0500

    Added some useful threads

--- src/main/java/com/blackrook/base/util/IOUtils.java
+++ src/main/java/com/blackrook/base/util/IOUtils.java
@@ -15,6 +15,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
 
 /**
  * Simple IO utility functions.
@@ -350,4 +354,264 @@ public final class IOUtils
 		try { c.close(); } catch (Exception e){}
 	}
 
+	/**
+	 * BufferedReader to Writer stream thread.
+	 * Transfers characters buffered at one line at a time until the source stream is closed.
+	 * <p> The thread terminates if the reader is closed. The target writer is not closed.
+	 */
+	public static class LineReaderToWriterThread extends Thread
+	{
+		protected BufferedReader sourceReader;
+		protected PrintWriter targetPrintWriter;
+		protected PrintStream targetPrintStream;
+		
+		private Throwable exception;
+		private long totalCharacters;
+		
+		private LineReaderToWriterThread(BufferedReader reader)
+		{
+			super("LineReaderToWriterThread");
+			this.sourceReader = reader;
+			this.exception = null;
+			this.totalCharacters = 0L;
+		}
+		
+		/**
+		 * Creates a new thread that, when started, transfers characters one line at a time until the source stream is closed.
+		 * The user must call {@link #start()} on this thread - it is not active after creation.
+		 * @param reader the Reader to read from (wrapped as a BufferedReader).
+		 * @param writer the Writer to write to.
+		 */
+		public LineReaderToWriterThread(Reader reader, Writer writer)
+		{
+			this(new BufferedReader(reader), new PrintWriter(writer));
+		}
+
+		/**
+		 * Creates a new thread that, when started, transfers characters one line at a time until the source stream is closed.
+		 * The user must call {@link #start()} on this thread - it is not active after creation.
+		 * @param reader the BufferedReader to read from.
+		 * @param writer the Writer to write to.
+		 */
+		public LineReaderToWriterThread(BufferedReader reader, PrintWriter writer)
+		{
+			this(reader);
+			this.targetPrintWriter = writer;
+		}
+
+		/**
+		 * Creates a new thread that, when started, transfers characters one line at a time until the source stream is closed.
+		 * The user must call {@link #start()} on this thread - it is not active after creation.
+		 * @param reader the BufferedReader to read from.
+		 * @param stream the PrintStream to write to.
+		 */
+		public LineReaderToWriterThread(BufferedReader reader, PrintStream stream)
+		{
+			this(reader);
+			this.targetPrintStream = stream;
+		}
+
+		@Override
+		public final void run() 
+		{
+			String line;
+			try {
+				while ((line = sourceReader.readLine()) != null)
+				{
+					if (targetPrintWriter != null)
+					{
+						targetPrintWriter.append(line).append('\n').flush();
+					}
+					else
+					{
+						targetPrintStream.println(line);
+						targetPrintStream.flush();
+					}
+					totalCharacters += line.length() + 1;
+				}
+			} catch (Throwable e) {
+				exception = e;
+			} finally {
+				afterClose();
+			}
+		}
+		
+		/**
+		 * Called after the source stream hits EOF or closes,
+		 * but before the Thread terminates.
+		 * <p> Does nothing by default.
+		 */
+		public void afterClose()
+		{
+			// Do nothing by default.
+		}
+		
+		/**
+		 * @return the exception that occurred, if any.
+		 */
+		public Throwable getException() 
+		{
+			return exception;
+		}
+		
+		/**
+		 * @return the total amount of characters moved.
+		 */
+		public long getTotalCharacters() 
+		{
+			return totalCharacters;
+		}
+		
+	}
+	
+	/**
+	 * Reader to Writer stream thread.
+	 * Transfers characters until the source stream is closed.
+	 * <p> The thread terminates if the reader is closed. The target writer is not closed.
+	 */
+	public static class ReaderToWriterThread extends Thread
+	{
+		protected Reader sourceReader;
+		protected Writer targetWriter;
+		
+		private Throwable exception;
+		private long totalCharacters;
+		
+		/**
+		 * Creates a new thread that, when started, transfers characters until the source stream is closed.
+		 * The user must call {@link #start()} on this thread - it is not active after creation.
+		 * @param reader the Reader to read from.
+		 * @param writer the Writer to write to.
+		 */
+		public ReaderToWriterThread(Reader reader, Writer writer)
+		{
+			super("ReaderToWriterThread");
+			this.sourceReader = reader;
+			this.targetWriter = writer;
+			this.exception = null;
+			this.totalCharacters = 0L;
+		}
+
+		@Override
+		public final void run() 
+		{
+			int buf;
+			char[] buffer = new char[8192]; 
+			try {
+				while ((buf = sourceReader.read(buffer)) > 0)
+				{
+					targetWriter.write(buffer, 0, buf);
+					targetWriter.flush();
+					totalCharacters += buf;
+				}
+			} catch (Throwable e) {
+				exception = e;
+			} finally {
+				afterClose();
+			}
+		}
+		
+		/**
+		 * Called after the source stream hits EOF or closes,
+		 * but before the Thread terminates.
+		 * <p> Does nothing by default.
+		 */
+		public void afterClose()
+		{
+			// Do nothing by default.
+		}
+		
+		/**
+		 * @return the exception that occurred, if any.
+		 */
+		public Throwable getException() 
+		{
+			return exception;
+		}
+		
+		/**
+		 * @return the total amount of characters moved.
+		 */
+		public long getTotalCharacters() 
+		{
+			return totalCharacters;
+		}
+		
+	}
+	
+	/**
+	 * Input to Output stream thread.
+	 * Transfers until the source stream is closed.
+	 * <p> The thread terminates if the input stream is closed. The target output stream is not closed.
+	 */
+	public static class InputToOutputStreamThread extends Thread
+	{
+		protected InputStream sourceStream;
+		protected OutputStream targetStream;
+		
+		private Throwable exception;
+		private long totalBytes;
+		
+		/**
+		 * Creates a new thread that, when started, transfers bytes until the source stream is closed.
+		 * The user must call {@link #start()} on this thread - it is not active after creation.
+		 * @param sourceStream the InputStream to read from. 
+		 * @param targetStream the OutputStream to write to.
+		 */
+		public InputToOutputStreamThread(InputStream sourceStream, OutputStream targetStream)
+		{
+			super("InputToOutputStreamThread");
+			this.sourceStream = sourceStream;
+			this.targetStream = targetStream;
+			this.exception = null;
+			this.totalBytes = 0L;
+		}
+		
+		@Override
+		public final void run() 
+		{
+			int buf;
+			byte[] buffer = new byte[8192]; 
+			try {
+				while ((buf = sourceStream.read(buffer)) > 0)
+				{
+					targetStream.write(buffer, 0, buf);
+					targetStream.flush();
+					totalBytes += buf;
+				}
+			} catch (Throwable e) {
+				exception = e;
+			} finally {
+				afterClose();
+			}
+		}
+		
+		/**
+		 * Called after the source stream hits EOF or closes,
+		 * but before the Thread terminates.
+		 * <p> Does nothing by default.
+		 */
+		public void afterClose()
+		{
+			// Do nothing by default.
+		}
+		
+		/**
+		 * @return the exception that occurred, if any.
+		 */
+		public Throwable getException() 
+		{
+			return exception;
+		}
+		
+		/**
+		 * @return the total amount of bytes moved.
+		 */
+		public long getTotalBytes() 
+		{
+			return totalBytes;
+		}
+		
+	}
+	
 }
